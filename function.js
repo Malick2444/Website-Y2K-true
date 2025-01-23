c5c78d9cd742fc25d7cddcbf5cb5db826f5edfb5
Malick Dia <malick.dia@student.ehb.be>
2025-01-23 15:35:46 +0100

    buttons and text done

--- function.js
+++ function.js
@@ -35,25 +35,25 @@ let currentIndex = 0; // Keeps track of the current image index
 const images = [
 	{
 		src: "images/senior_1.png",
-		title: "MATTEO  GIAMBARRES  2MCT",
+		title: "MATTEO  GIAMBARRES  STUDENT 2MCT",
 		description:
 			"Een erg interessante studierichting waar je veel kunt leren over diverse domeinen. Zo ontdek je wat je leuk vindt en kun je in het tweede jaar kiezen waarin je je wilt specialiseren.",
 	},
 	{
 		src: "images/senior_2.png",
-		title: "RAMON DE&nbsp;RAES 2MCT",
+		title: "RAMON DE&nbsp;RAES STUDENT&nbsp;2MCT",
 		description:
 			"Een boeiende en veelzijdige studierichting waarin je een brede waaier aan vakken verkent. Ideaal om je interesses te ontdekken en jezelf breed te ontwikkelen!",
 	},
 	{
 		src: "images/senior_3.png",
-		title: "EREN SAHBAZ 2MCT",
+		title: "EREN SAHBAZ STUDENT&nbsp;2MCT",
 		description:
-			"Een leuke en uitdagende richting waar je niet alleen verschillende vakken verkent, maar ook werkt aan praktische vaardigheden zoals back-end ontwikkeling en het gebruik van technieken zoals async/await. Samenwerken met klasgenoten maakt het leerproces nog leuker en biedt een goede voorbereiding op de toekomst! ",
+			"Een leuke en uitdagende richting waar je niet alleen verschillende vakken verkent, maar ook werkt aan praktische vaardigheden zoals back-end ontwikkeling en het gebruik van technieken zoals async/await.",
 	},
 	{
 		src: "images/senior_4.png",
-		title: "SOPHIA RAHMOUN 2CMT",
+		title: "SOPHIA RAHMOUN STUDENT&nbsp;2MCT",
 		description:
 			"Een brede en goed gestructureerde opleiding waarin je veel ervaring opdoet in diverse domeinen. Je leert in een fijne omgeving met enthousiaste medestudenten en inspirerende docenten die je motiveren om het beste uit jezelf te halen!",
 	},
